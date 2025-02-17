646a59fab16f58cb67a9559b570ef45c3a3472cf
Felipe Lourenzi <felipesiegle@gmail.com>
2025-02-17 08:29:07 -0300

    update: docker config

--- start.sh
+++ start.sh
@@ -11,10 +11,6 @@ composer install --no-interaction --prefer-dist --optimize-autoloader >> /var/ww
 echo "Executando as migrations..." >> /var/www/startup.log
 php artisan migrate >> /var/www/startup.log 2>&1
 
-# Rodar os testes e logar
-echo "Executando os testes..." >> /var/www/startup.log
-php artisan test >> /var/www/startup.log 2>&1
-
 # Iniciar o PHP-FPM
 echo "Iniciando PHP-FPM..." >> /var/www/startup.log
 php-fpm
